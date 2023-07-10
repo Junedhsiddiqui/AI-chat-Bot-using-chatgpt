7a8a9977dc2320140c917793a39223fb31fc7178
Junedh Siddiqui <91174575+Junedhsiddiqui@users.noreply.github.com>
2023-07-10 22:36:54 +0530

    Add files via upload

---
+++ openAI/02 chatgpt chat assistant copy.py
@@ -0,0 +1,18 @@
+import openai
+
+openai.api_key = "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"
+
+messages = []
+system_msg = input("What type of chatbot would you like to create?\n")
+messages.append({"role": "system", "content": system_msg})
+
+print("Your new assistant is ready!")
+while input != "quit()":
+    message = input()
+    messages.append({"role": "user", "content": message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages)
+    reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": reply})
+    print("\n" + reply + "\n")
